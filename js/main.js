afd8566173641b581755c12ae13eab661584fb29
nishitagiduturi <nishitaoct26@gmail.com>
2020-11-02 23:42:28 +0530

    Added smooth scrolling using jquery cdn

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Smooth scrolling
+$(".btn").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    var hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800,
+      function () {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
